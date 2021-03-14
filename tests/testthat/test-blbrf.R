9420c0f45c488c1c0d6e69aacf02b24eb3d6fcd5
David Mao <dlmao@ucdavis.edu>
2021-03-14 14:33:15 -0700

    fix coding style blbrf

--- tests/testthat/test-blbrf.R
+++ tests/testthat/test-blbrf.R
@@ -1,12 +1,12 @@
 context("test-blbrf.r")
 
 test_that("factor levels of tree are correct", {
-  model <- blbrf(Species ~ ., data = iris, m = 2, B = 500, nthreads = 1)
+  model <- blbrf(Species ~ ., data = iris, m = 2, B = 100, nthreads = 1)
   expect_identical(model$levels, levels(iris$Species))
 })
 
 test_that("ensure end probs are the mean prob of all models", {
-  model <- blbrf(Species ~ ., data = iris, m = 2, B = 500, nthreads = 1)
+  model <- blbrf(Species ~ ., data = iris, m = 2, B = 100, nthreads = 1)
   new_dat <- iris[1,-5]
   x1 = model$estimates$`1`
   predicted1 = predict(x1, new_dat)
